e6bd772ab439420297dda8ae4594958bc0425576
dexterchan <dexterchan@gmail.com>
2019-05-03 16:43:05 +0800

    add read

--- src/main/java/io/exp/apachebeam/kafka/RunBatchPipline.java
+++ src/main/java/io/exp/apachebeam/kafka/RunBatchPipline.java
@@ -9,17 +9,18 @@ import org.apache.beam.sdk.metrics.Distribution;
 import org.apache.beam.sdk.metrics.Metrics;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Create;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
-
+import com.google.common.collect.ImmutableMap;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 class Term implements Serializable {
@@ -73,15 +74,17 @@ public class RunBatchPipline {
 
     public static void main(String[] args){
         LOGGER.debug("Run the kafka connection");
-        KafkaProducerValuesExample.insertStrings();
+        //KafkaProducerValuesExample.insertStrings();
+        KafkaProducerValuesExample.readStrings();
     }
+    static final String TOKENIZER_PATTERN = "[^\\p{L}]+";
 
      static class KafkaProducerValuesExample {
         static final String[] WORDS_ARRAY = new String[] {
                 "hi there", "hi", "hi sue bob",
                 "hi sue", "", "bob hi"};
         static String BOOTSTRAPSERVER="localhost:9092";
-        static String TestTopic="test2";
+        static String TestTopic="test";
 
         static final List<String> WORDS = Arrays.asList(WORDS_ARRAY);
 
@@ -98,6 +101,51 @@ public class RunBatchPipline {
             );
 
             p.run().waitUntilFinish();
+        }
+
+        public static void readStrings(){
+            PipelineOptions options = PipelineOptionsFactory.create();
+
+            // Create the Pipeline object with the options we defined above.
+
+            Pipeline p = Pipeline.create(options);
+            p.apply(KafkaIO.<Long, String>read()
+                    .withBootstrapServers(BOOTSTRAPSERVER)
+                    .withTopic(TestTopic)
+                    .withKeyDeserializer(LongDeserializer.class)
+                    .withValueDeserializer(StringDeserializer.class)
+
+                    .updateConsumerProperties(ImmutableMap.of("auto.offset.reset", (Object)"earliest"))
+
+                    // We're writing to a file, which does not support unbounded data sources. This line makes it bounded to
+                    // the first 5 records.
+                    // In reality, we would likely be writing to a data source that supports unbounded data, such as BigQuery.
+                    .withMaxNumRecords(5)
+
+                    .withoutMetadata() // PCollection<KV<Long, String>>
+            )
+                    .apply(Values.<String>create())
+                    .apply("ExtractWords", ParDo.of(new DoFn<String, String>() {
+                        @ProcessElement
+                        public void processElement(ProcessContext c) {
+                            for (String word : c.element().split(TOKENIZER_PATTERN)) {
+                                if (!word.isEmpty()) {
+                                    c.output(word);
+                                }
+                            }
+                        }
+                    }))
+                    .apply(Count.<String>perElement())
+                    .apply("FormatResults", MapElements.via(new SimpleFunction<KV<String, Long>, String>() {
+                        @Override
+                        public String apply(KV<String, Long> input) {
+                            return input.getKey() + ": " + input.getValue();
+                        }
+                    }))
+                    .apply(TextIO.write().to("wordcounts"));
+
+            p.run().waitUntilFinish();
+
         }
     }
 }
